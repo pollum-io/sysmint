332b887f163969c41870732e1a28a2cbdd0e4652
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-11-05 22:47:02 -0300

    fix some issues disabling extension

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,31 +1,23 @@
 import { useSelector } from "react-redux";
-import Bowser from "bowser";
 
-import store from "../state/store";
-import setupState from "../utils/setupState";
+import { setupState } from "../utils/setupState";
 
 export default function Home() {
-  const controller = useSelector((state) => state.controller);
   const isLocked = useSelector((state) => state.isLocked);
+  const controller = useSelector((state) => state.controller);
   const isInstalled = useSelector((state) => state.isInstalled);
   const isConnected = useSelector((state) => state.connected);
 
   const handleConnect = async (event) => {
     event.preventDefault();
 
-    const browser = Bowser.getParser(window.navigator.userAgent);
-
     if (!isInstalled) {
-      if (browser.getBrowserName() === "Firefox") {
-        return window.open("https://addons.mozilla.org/en-US/firefox/addon/pali-wallet/", "_blank")
-      }
-
-      return window.open("https://chrome.google.com/webstore/detail/pali-wallet/mgffkfbidihjpoaomajlbgchddlicgpn?hl=en-US", "_blank")
+      return window.open("https://chrome.google.com/webstore/detail/pali-wallet/mgffkfbidihjpoaomajlbgchddlicgpn?hl=en-US");
     }
 
-    if (controller) {
-      controller.connectWallet().then(async (response) => {
-        response && (await setupState(store));
+    if (controller !== null) {
+      controller.connectWallet().then(() => {
+        setupState();
       });
     }
   };
@@ -48,7 +40,7 @@ export default function Home() {
             >
               {isInstalled
                 ? (isLocked && isConnected)
-                  ? "Unlock Pali wallet"
+                  ? "Unlock Pali Wallet"
                   : "Connect to Pali Wallet"
                 : "Install Pali Wallet"}
             </button>
